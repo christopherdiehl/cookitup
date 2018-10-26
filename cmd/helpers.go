2d33d0fb78ac5fcc7881f94f50f9524819708175
Christopher <diehl.chris24@gmail.com>
2018-10-26 17:02:23 -0400

    Commit the cobra boilerplate

---
+++ cmd/helpers.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+func PromptSTDIN(reader *bufio.Reader, message string) (userInput string, err error) {
+	fmt.Println(message)
+	userInput, err = reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	userInput = strings.Replace(userInput, "\n", "", -1)
+	return
+}
